350b76fda13cc020ff4fc1fd01e57f1e06c3014c
mutularyan <162291833+mutularyan@users.noreply.github.com>
2024-06-27 22:10:34 +0300

    Update scrabble.py

--- scrabble.py
+++ scrabble.py
@@ -1,10 +1,68 @@
 import random
 
-# Initialize the board (15x15 grid)
 board = [[" " for _ in range(15)] for _ in range(15)]
 board[7][7] = "  X "
+board[0][0] = " TW "
+board[0][3] = " DL "
+board[0][7] = " TW "
+board[0][11] = " DL "
+board[0][14] = " TW " 
+board[1][1] = " DW "
+board[1][5] = " TL "
+board[1][9] = " TL "
+board[1][13] = " DW "
+board[2][2] = " DW "
+board[2][6] = " DL"
+board[2][8] = " DL " 
+board[2][12] = " DW "
+board[3][0] = " DL "
+board[3][3] = " DW "
+board[3][7] = " DL "
+board[3][11] = " DW "
+board[3][14] = " DL "
+board[4][4] = " DW " 
+board[4][10] = " DW "
+board[5][1] = " TL "
+board[5][5] = " TL "
+board[5][9] = " TL "
+board[5][13] = " TL "
+board[6][2] = " DL"
+board[6][6] = " DL " 
+board[6][8] = " DL "
+board[6][12] = " DL "
+board[7][0] = " TW "
+board[7][3] = " DL "
+board[7][11] = " DL "
+board[7][14] = " TW "
+board[8][2] = " DL " 
+board[8][6] = " DL "
+board[8][8] = " DL "
+board[8][12] = " DL "
+board[9][1] = " TL "
+board[9][5] = " TL "
+board[9][9] = " TL"
+board[9][13] = " TL " 
+board[10][4] = " DW "
+board[10][10] = " DW "
+board[11][0] = " DL "
+board[11][3] = " DW "
+board[11][7] = " DL "
+board[11][11] = " DW "
+board[11][14] = " DL " 
+board[12][2] = " DW "
+board[12][6] = " DL "
+board[12][8] = " DL "
+board[12][12] = " DW "
+board[13][1] = " DW "
+board[13][5] = " TL"
+board[13][9] = " TL " 
+board[13][13] = " DW "
+board[14][0] = " TW "
+board[14][3] = " DL"
+board[14][7] = " TW " 
+board[14][11] = " DL "
+board[14][14] = " TW "
 
-# Example letter points dictionary
 letter_points = {
     'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1,
     'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8,
@@ -12,8 +70,18 @@ letter_points = {
     'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1,
     'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10
 }
+ 
+letter_no = {
+    'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12,
+    'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1,
+    'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8,
+    'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6,
+    'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
+}
+letter_bag = []
 
-letter_bag = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ" * 2)
+for letter, count in letter_no.items():
+    letter_bag.extend([letter] * count)
 random.shuffle(letter_bag)
 player_rack = [letter_bag.pop() for _ in range(7)]
 computer_rack = [letter_bag.pop() for _ in range(7)]
@@ -21,7 +89,7 @@ computer_rack = [letter_bag.pop() for _ in range(7)]
 def display_board():
     cell_width = 4
     
-    header = "   " + " | ".join(f"{i:<{cell_width}}" for i in range(15)) + " |"
+    header = "     " + " | ".join(f"{i:<{cell_width}}" for i in range(15)) + " |"
     separator = "  " + "+".join("-" * (cell_width + 2) for _ in range(15)) + "+"
 
     board_str = header + "\n" + separator + "\n"
