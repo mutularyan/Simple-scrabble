dddbe4a9002bfcecb9dfeb8b8c8649ca0c7c1a21
NatashaMakungu <161466937+NatashaMakungu@users.noreply.github.com>
2024-07-16 16:30:34 +0300

    Added game panel in Game/index.js, edit where necessary

--- Frontend/src/APP/Pages/Game/index.jsx
+++ Frontend/src/APP/Pages/Game/index.jsx
@@ -1,9 +1,17 @@
-function Game () {
-    return (
-        <div>
-            <h1>My Game</h1>
+import React from 'react';
+
+function Game() {
+  return (
+    <div className="game-screen">
+      <div className="message-panel">
+        <p className="welcome-message">Welcome Player: //user name</p>
+        <div className="button-group">
+          <button className="logout-button">Logout</button>
+          <button className="new-game-button">New Game</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Game;
