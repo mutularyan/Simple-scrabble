15b92418df39e9e09661d72b86fe30786b5cfadf
mutularyan <162291833+mutularyan@users.noreply.github.com>
2024-07-18 14:16:49 +0300

    Update Game page

--- Frontend/src/APP/Pages/Game/index.jsx
+++ Frontend/src/APP/Pages/Game/index.jsx
@@ -1,6 +1,47 @@
 import React from 'react';
+// import APPCONTEXT from 'Frontend/src/APP/Context/AppContext.JSX'
+// import axios from "axios";
+// import { useContext, useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Board from "./Board"
 
 function Game() {
+    // const { user, token } = useContext(APPCONTEXT);
+    // const navigate = useNavigate();
+    // const [board, setBoard] = useState([]);
+    // console.log(board);
+  
+    // useEffect(() => {
+      // if (!user || !token) {
+        // navigate("/");
+        // return;
+      // }
+      // getboard();
+    // }, []);
+  
+    // const getboard = () => {
+      // axios({
+        // method: "GET",
+        // url: "http://127.0.0.1:9000/game/board",
+        // headers: {
+          // Authorization: `Bearer ${token}`,
+        // },
+      // })
+        // .then((res) => {
+          // setBoard(res?.data?.board || []);
+        // })
+        // .catch((err) => {
+          // if (!user || !token) {
+            // navigate("/");
+            // return;
+          // }
+        // });
+    // };
+
+
+
+
+
   return (
     <div className="game-screen">
       <div className="message-panel">
@@ -8,6 +49,9 @@ function Game() {
         <div className="button-group">
           <button className="logout-button">Logout</button>
           <button className="new-game-button">New Game</button>
+          <div>
+            {/* <Board board={board} setBoard={setBoard} getboard={getboard}/> */}
+          </div>
         </div>
       </div>
     </div>
