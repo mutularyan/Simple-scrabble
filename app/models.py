8f057ecc35c5ca206efe4c57059472c83d046be2
NatashaMakungu <161466937+NatashaMakungu@users.noreply.github.com>
2024-07-18 23:09:15 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -19,19 +19,3 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     member_id = db.Column(db.BigInteger,db.ForeignKey('member.id', ondelete = 'CASCADE'), nullable=False,unique=True) 
     board=db.Column(db.Text,nullable=False)
-    player_rack = db.Column(db.Text, nullable=False) 
-    tile_bag = db.Column(db.Text, nullable=False)
-    player_score = db.Column(db.Integer, nullable=False, default=0)
-    computer_rack = db.Column(db.Text, nullable=False)
-    computer_score = db.Column(db.Integer, nullable=False, default=0)
-
-
-    def game_state(self):
-        return {
-            'board': self.board,
-            'player_rack': self.player_rack,
-            'tile_bag': self.tile_bag,
-            'player_score': self.player_score,
-            'computer_rack': self.computer_rack,
-            'computer_score': self.computer_score
-        }
